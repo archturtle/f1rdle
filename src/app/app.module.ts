787cc8f71792e898077def23488f64fb09271464
Siddhartha Juluru <31663833+archturtle@users.noreply.github.com>
2023-02-25 13:10:58 -0500

    Folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { GuessComponent } from './components/guess/guess.component';
+import { GuessesComponent } from './components/guesses/guesses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    StatisticsComponent,
+    GuessComponent,
+    GuessesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/guesses/guesses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guesses',
+  templateUrl: './guesses.component.html',
+  styleUrls: ['./guesses.component.css']
+})
+export class GuessesComponent {
+
+}
