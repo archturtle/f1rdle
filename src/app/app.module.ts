3048e5c4c109df376f6c6114cf3f823b91c0a0f2
Siddhartha Juluru <31663833+archturtle@users.noreply.github.com>
2023-02-25 16:00:18 -0500

    created the statistics component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { GuessComponent } from './components/guess/guess.component';
 import { GuessesComponent } from './components/guesses/guesses.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { GuessesComponent } from './components/guesses/guesses.component';
   ],
   imports: [
     HttpClientModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
