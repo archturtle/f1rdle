a2a0d94497130eabb2bf18ea07ec6532cb2c2033
Evan Prizel <emp4506@rit.edu>
2023-02-25 15:23:19 -0500

    Finished the routing and button needed for the navigation dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'f1rdle';
+  title = "f1rdle";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from "@angular/material/button";
+
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { GuessComponent } from './components/guess/guess.component';
 import { GuessesComponent } from './components/guesses/guesses.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EasyPageComponent } from './components/easy-page/easy-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,10 +18,14 @@ import { GuessesComponent } from './components/guesses/guesses.component';
     DashboardComponent,
     StatisticsComponent,
     GuessComponent,
-    GuessesComponent
+    GuessesComponent,
+    EasyPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
