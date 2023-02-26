0a2b4c370ad5393be6224c91935feb60b90f276e
Siddhartha Juluru <31663833+archturtle@users.noreply.github.com>
2023-02-26 00:54:53 -0500

    Deleted unused components and changed the nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {EasyPageComponent} from "./components/easy-page/easy-page.component";
-import {MediumPageComponent} from "./components/medium-page/medium-page.component";
-import {HardPageComponent} from "./components/hard-page/hard-page.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/', pathMatch: 'full'},
-  {path: 'easy', component: EasyPageComponent},
-  {path: 'medium', component: MediumPageComponent},
-  {path: 'hard', component: HardPageComponent},
-  {path: '**', redirectTo: '/', pathMatch: 'full'}
+  {path: '**', redirectTo: '/', pathMatch: 'full'},
+  // {path: 'about', component: HardPageComponent},
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,7 @@ import { StatisticsComponent } from './components/statistics/statistics.componen
 import { GuessComponent } from './components/guess/guess.component';
 import { GuessesComponent } from './components/guesses/guesses.component';
 import { AppRoutingModule } from './app-routing.module';
-import { EasyPageComponent } from './components/easy-page/easy-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MediumPageComponent } from './components/medium-page/medium-page.component';
-import { HardPageComponent } from './components/hard-page/hard-page.component';
 
 import { CircuitsService } from 'src/app/services/circuits.service';
 import { firstValueFrom } from 'rxjs';
@@ -30,9 +27,6 @@ import {ReactiveFormsModule} from "@angular/forms";
     StatisticsComponent,
     GuessComponent,
     GuessesComponent,
-    EasyPageComponent,
-    MediumPageComponent,
-    HardPageComponent,
   ],
   imports: [
     FormsModule,
